6dcc398222545d2e119b4b14a666baaa26a7d78c
Edward Andrews-Hodgson <edward@aldreth.com>
2020-05-22 13:02:04 +0100

    Change cookie info (#218)
    Co-authored-by: Edward Andrews-Hodgson <edward.andrewshodgson@anaplan.com>

--- src/components/CookieDialog/cookieDialogSlice.ts
+++ src/components/CookieDialog/cookieDialogSlice.ts
@@ -1,16 +1,11 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 interface CookieDialogState {
-  viewed: boolean;
   open: boolean;
 }
 
-interface CookieDialogViewedPayload {
-  viewed: boolean;
-}
 
 export const cookieDialogInitialState: CookieDialogState = {
-  viewed: false,
   open: false
 };
 
@@ -18,16 +13,12 @@ const cookieDialogSlice = createSlice({
   name: "cookieDialog",
   initialState: cookieDialogInitialState,
   reducers: {
-    setViewed(state, action: PayloadAction<CookieDialogViewedPayload>) {
-      const { viewed } = action.payload;
-      state.viewed = viewed;
-    },
     toggleOpen(state) {
       state.open = !state.open;
     }
   }
 });
 
-export const { setViewed, toggleOpen } = cookieDialogSlice.actions;
+export const { toggleOpen } = cookieDialogSlice.actions;
 
 export default cookieDialogSlice.reducer;
