1bb10e5835ebf09d2421bd73d3413f4b235f6c54
Edward Andrews-Hodgson <edward@aldreth.com>
2021-04-08 19:54:20 +0100

    try (#588)

--- netlify/functions/addresses.js
+++ netlify/functions/addresses.js
@@ -1,25 +1,51 @@
 const fetch = require("node-fetch");
 
-exports.handler = async function (event, context) {
+const API_ENDPOINT = "https://cat-fact.herokuapp.com/facts";
+
+exports.handler = async (event, context) => {
+  let response;
   try {
-    const response = await fetch(
-      "https://addresses.york.gov.uk/api/address/lookupbypostcode/YO24%201DD",
-      {
-        method: "GET",
-        headers: {},
-      }
-    );
+    response = await fetch(API_ENDPOINT);
+    // handle response
+  } catch (err) {
     return {
-      statusCode: 200,
-      body: JSON.stringify(response),
-    };
-  } catch (error) {
-    const { statusCode = 500, message = "Something's gone wrong" } = error;
-    return {
-      statusCode,
+      statusCode: err.statusCode || 500,
       body: JSON.stringify({
-        error: message,
+        error: err.message,
       }),
     };
   }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: response,
+    }),
+  };
 };
+
+// const fetch = require("node-fetch");
+
+// exports.handler = async function (event, context) {
+//   try {
+//     const response = await fetch(
+//       "https://addresses.york.gov.uk/api/address/lookupbypostcode/YO24%201DD",
+//       {
+//         method: "GET",
+//         headers: {},
+//       }
+//     );
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(response),
+//     };
+//   } catch (error) {
+//     const { statusCode = 500, message = "Something's gone wrong" } = error;
+//     return {
+//       statusCode,
+//       body: JSON.stringify({
+//         error: message,
+//       }),
+//     };
+//   }
+// };
