8122b4aa26077257f66bf58cf4df70e27875d171
Edward Andrews-Hodgson <edward@aldreth.com>
2021-04-08 19:45:54 +0100

    Stringify response

--- netlify/functions/addresses.js
+++ netlify/functions/addresses.js
@@ -1,17 +1,25 @@
 const fetch = require("node-fetch");
 
 exports.handler = async function (event, context) {
-  fetch(
-    "https://addresses.york.gov.uk/api/address/lookupbypostcode/YO24%201DD",
-    {
-      method: "GET",
-      headers: {},
-    }
-  )
-    .then((response) => {
-      return response;
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  try {
+    const response = await fetch(
+      "https://addresses.york.gov.uk/api/address/lookupbypostcode/YO24%201DD",
+      {
+        method: "GET",
+        headers: {},
+      }
+    );
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    };
+  } catch (error) {
+    const { statusCode = 500, message = "Something's gone wrong" } = error;
+    return {
+      statusCode,
+      body: JSON.stringify({
+        error: message,
+      }),
+    };
+  }
 };
