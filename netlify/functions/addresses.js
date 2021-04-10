a1f87fe04a7a06837ae120901fd9d52e94ec6ad2
Edward Andrews-Hodgson <edward@aldreth.com>
2021-04-10 18:13:24 +0100

    body (#594)

--- netlify/functions/addresses.js
+++ netlify/functions/addresses.js
@@ -4,12 +4,16 @@ const API_ENDPOINT =
   "https://addresses.york.gov.uk/api/address/lookupbypostcode/YO24%201DD";
 
 export async function handler(event, context) {
+  const body = JSON.parse(event.body);
+  console.log("*********************", body);
+
   try {
     const response = await fetch(API_ENDPOINT);
     const json = await response.json();
+    console.log("***************", json);
     return {
       statusCode: 200,
-      body: JSON.stringify(json),
+      body: JSON.stringify(body),
     };
   } catch (err) {
     console.error(err);
