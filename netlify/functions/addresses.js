d35ed0c53be6ba863ebadf802d5a1d6d6eba6002
Edward Andrews-Hodgson <edward@aldreth.com>
2021-04-10 18:22:55 +0100

    fix (#596)

--- netlify/functions/addresses.js
+++ netlify/functions/addresses.js
@@ -1,20 +1,20 @@
 import fetch from "node-fetch";
 
 const API_ENDPOINT =
-  "https://addresses.york.gov.uk/api/address/lookupbypostcode/YO24%201DD";
+  "https://addresses.york.gov.uk/api/address/lookupbypostcode/";
 
 export async function handler(event, context) {
   const body = JSON.parse(event.body);
   console.log("*********************", body);
+  const encodedPostcode = encodeURIComponent("YO24 1DD");
 
   try {
-    const response = await fetch(API_ENDPOINT);
+    const response = await fetch(`${API_ENDPOINT}${encodedPostcode}`);
     const json = await response.json();
     console.log("***************", json);
     return {
       statusCode: 200,
-      body,
-      // body: JSON.stringify(body),
+      body: JSON.stringify(body),
     };
   } catch (err) {
     console.error(err);
